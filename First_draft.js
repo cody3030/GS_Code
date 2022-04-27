94b63c3b09fff039ebcffc36c815cfa5d95495b0
Cody3030 <watsoncody89@gmail.com>
2022-04-26 22:47:56 -0400

    more comments and replaced numbers with variables

--- First_draft.js
+++ First_draft.js
@@ -1,7 +1,11 @@
 //Declaring variables
-var my_text = text("0", 10,10, "Green")
-var points = 0;
-var targets = [];
+var my_text = text("0", 10,10, "Green") // The text object displaying the number of points.
+var points = 0; // The number of points the player has.
+var targets = [];  // An array for our targets.
+var target_creation_rate = 45; // Rate at which we will create targets, default every 45 milliseconds
+var target_movement_rate = 8; // Rate at which the targets will update their Y position, every 8 milliseconds
+var target_velocity = 10; // The amount the target's Y position will increase per loop, 10 pixels.
+var collision_rate = 3; // The rate at which we calculate collisions, 3 milliseconds.
 
 //target creation loop
 repeat(function(){
@@ -9,13 +13,13 @@ repeat(function(){
   let temp_circle = circle(Math.random()*width, 0, Math.random()*40+10);
   targets.push(temp_circle);
   }
-},45);
+},target_creation_rate);
 
 //Target movement loop
 repeat(function(){
   let to_remove= null;
   for(let i=0; i < targets.length; i++){
-    targets[i].y = targets[i].y + 10; //Increasing the Y position of the target moves it down the screen.
+    targets[i].y += target_velocity; //Increasing the Y position of the target moves it down the screen.
     if(targets[i].y > height+targets[i].radius){ //Any targets past the bottom should be ignored.
       to_remove = i;
     }
@@ -24,7 +28,7 @@ repeat(function(){
     targets[to_remove] = undefined;
     targets.splice(to_remove,1);
   }
-},8);
+},target_movement_rate);
 
 // A helper class
 class Point {
@@ -63,4 +67,4 @@ repeat(function(){
       return;
     }
   }
-},3);
+},collision_rate);
